1dc538465ba68ae6a5597d73e72d4f7fc728522a
Niranjan Rajkumar <128575600+Niranjanr0682@users.noreply.github.com>
2024-07-04 14:52:38 +0530

    4/7/24

---
+++ juzzzz/Strcmp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Strcmp
+{public static void main(String[] args)
+    {
+        Scanner monk=new Scanner(System.in);
+        System.out.print("Enter string 1 :");
+        String s1=monk.nextLine();
+        System.out.print("Enter string 2:");
+        String s2=monk.nextLine();
+        int n = s1.compareTo(s2);
+        if(n==0)
+        {System.out.println("Both strings are equal");}
+        else if (n>0)
+        {System.out.println("s1 is greater than s2");}
+        else
+        {System.out.println("s2 is greater than s1");}}}
+                
---
+++ juzzzz/Strlen.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Strlen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s1 = sc.nextLine();
+        System.out.println("The Length is "+s1.length());}}
+        
